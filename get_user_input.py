1891bc11bfcea888f922688eb166b972ea3abfb8
William <william.deldesalojo@gmail.com>
2021-03-16 00:00:47 +0100

    made it so the numbers were ints

--- get_user_input.py
+++ get_user_input.py
@@ -1,9 +1,9 @@
 
 
-# Get The First Number and Convert To Int
+# Get The First Number and Convert To Integer
 num1 = int(input("Please Give The Lowest Number: >>> "))
 
-# Get The Second Number and Convert To Int
+# Get The Second Number and Convert To Integer
 num2 = int(input("Please Give The Highest Number: >>> "))
 
 # Get The Sum
@@ -14,11 +14,13 @@ if (num1 < num2):
     print(num1, num2)
     print("Total", sum_of_numbers)
 
+# If Number 1 is equal to Number 2
 elif (num1 == num2):
     print(num1, num2)
     print("Total", sum_of_numbers)
     print("Both Numbers Are Equal Btw")
 
+# If number 1 is more than number 2 flip order, and tell the user about the error
 else:
     print(num2, num1)
     print("Total", sum_of_numbers)
